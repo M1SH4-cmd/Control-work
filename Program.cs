7735cfe7ee9a7bef1107a59f0e0314bf4ea35a99
M1SH4-cmd <6el0zu61979@mail.ru>
2023-12-14 00:13:30 +0700

    GG

--- Program.cs
+++ Program.cs
@@ -1,16 +1,21 @@
-﻿class Program
+﻿using System;
+class Program
 {
     static void Main(string[] args)
     {
     string[] array1 = ["Hola, Amigo", "Espanola", "RIO", "R&B", "OG Michael"];
     string[] result = new string[array1.Length];
     int count = 0;
-    void Choice()
+
+    foreach (string s in array1)
     {
-        for(int i = 0; int n<=5; i++)
+        if(s.Lenght <= 3)
         {
-            if()
+
+            result[count] = s;
+            count++;
         }
     }
+    Array.Resize(ref result; count)
     }
 }
