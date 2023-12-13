e2e4fd3f05b5116de39ad2617206100668dd0da1
M1SH4-cmd <6el0zu61979@mail.ru>
2023-12-14 00:05:57 +0700

    Added more code

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 ﻿class Program
 {
-    string Array1 = ["Hola, Amigo", "Espanola", "RIO", "R&B", "OG Michael"];
+    static void Main(string[] args)
+    {
+    string[] array1 = ["Hola, Amigo", "Espanola", "RIO", "R&B", "OG Michael"];
+    string[] result = new string[array1.Length];
+    int count = 0;
     void Choice()
     {
         for(int i = 0; int n<=5; i++)
         {
             if()
         }
+    }
     }
 }
